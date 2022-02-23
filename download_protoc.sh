a037a56bbb9179f7b1b1dafbd5746ae7f6cb1ca5
gitJaesik <maguire1815@gmail.com>
2022-02-23 20:14:17 +0900

    Feat: Add protoc downloader

---
+++ download_protoc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# linux
+# PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+# curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+
+# unzip protoc-3.15.8-linux-x86_64.zip
+
+# max
+brew install protobuf
