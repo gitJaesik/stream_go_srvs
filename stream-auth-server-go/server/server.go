1a0b2c68757ceee48f73f36c28ce22ca20fc240c
gitJaesik <maguire1815@gmail.com>
2022-05-05 01:12:11 +0900

    Refactor: create gateway mux method

--- stream-auth-server-go/server/server.go
+++ stream-auth-server-go/server/server.go
@@ -1,14 +1,19 @@
 package server
 
 import (
+	"context"
 	"crypto/tls"
+	"log"
 
+	_ "github.com/gitJaesik/stream_go_srvs/stream-auth-server-go/test/testdata"
 	_ "github.com/gitJaesik/stream_go_srvs/streamgolib/config" // for save
 	_ "github.com/gitJaesik/stream_go_srvs/streamgolib/data"   // for save
 	"github.com/gitJaesik/stream_go_srvs/streamgolib/logger"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
 	"github.com/gitJaesik/stream_go_srvs/stream-auth-server-go/config"
 	"github.com/gitJaesik/stream_go_srvs/stream-auth-server-go/service"
+	"github.com/gitJaesik/stream_go_srvs/stream-auth-server-go/test/testdata"
 	sglconfig "github.com/gitJaesik/stream_go_srvs/streamgolib/config"
 	"github.com/gitJaesik/stream_go_srvs/streamgolib/data"
 	sgldb "github.com/gitJaesik/stream_go_srvs/streamgolib/db"
@@ -17,6 +22,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var Set = wire.NewSet(
@@ -26,9 +32,28 @@ var Set = wire.NewSet(
 // InitializeGrpcServer ...
 func InitializeGrpcServer(sglConfig *config.SGLConfig, mongoDb sgldb.StreamGoLibDB) *grpc.Server {
 
+	streamServerInterceptor := NewStreamServerInterceptor()
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(streamServerInterceptor.AuthUnaryInterceptorHandler),
+	}
+
+	// Create a gRPC server object
+	grpcServer := grpc.NewServer(opts...)
+
+	// Attach the ... service to the server
+	pbsas.RegisterStreamAuthServiceServer(grpcServer, service.NewStreamAuthServerServer(mongoDb))
+
+	return grpcServer
+}
+
+// InitializeSecureGrpcServer ...
+func InitializeSecureGrpcServer(sglConfig *config.SGLConfig, mongoDb sgldb.StreamGoLibDB) *grpc.Server {
+
 	cert, err := tls.LoadX509KeyPair(data.Path(sglconfig.GetCertFile()), data.Path(sglconfig.GetKeyFile()))
 	if err != nil {
 		logger.Logger.Errorw("InitializeGrpcServer", "failed to load key pair", err)
+		panic(err)
 	}
 	logger.Logger.Infow("InitializeGrpcServer", "sgldata.Path(sglconfig.GetCertFile())", data.Path(sglconfig.GetCertFile()))
 	logger.Logger.Infow("InitializeGrpcServer", "sgldata.Path(sglconfig.GetKeyFile())", data.Path(sglconfig.GetKeyFile()))
@@ -40,12 +65,6 @@ func InitializeGrpcServer(sglConfig *config.SGLConfig, mongoDb sgldb.StreamGoLib
 		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
 	}
 
-	// streamServerInterceptor := NewStreamServerInterceptor()
-
-	// opts := []grpc.ServerOption{
-	// 	grpc.UnaryInterceptor(streamServerInterceptor.AuthUnaryInterceptorHandler),
-	// }
-
 	// Create a gRPC server object
 	grpcServer := grpc.NewServer(opts...)
 
@@ -53,4 +72,57 @@ func InitializeGrpcServer(sglConfig *config.SGLConfig, mongoDb sgldb.StreamGoLib
 	pbsas.RegisterStreamAuthServiceServer(grpcServer, service.NewStreamAuthServerServer(mongoDb))
 
 	return grpcServer
+}
+
+// InitializeGatewayMux ...
+func InitializeGatewayMux() *runtime.ServeMux {
+
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{
+		grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.DialContext(context.Background(), "0.0.0.0:8280", opts...)
+	if err != nil {
+		logger.Logger.Errorw("Failed to dial server", "failed to load key pair", err)
+		panic(err)
+	}
+
+	err = pbsas.RegisterStreamAuthServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	return mux
+}
+
+// InitializeSecureGatewayMux ...
+func InitializeSecureGatewayMux() *runtime.ServeMux {
+
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	creds, err := credentials.NewClientTLSFromFile(testdata.Path("x509/ca_cert.pem"), "x.test.example.com")
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithBlock(), grpc.WithTransportCredentials(creds),
+	}
+
+	conn, err := grpc.DialContext(context.Background(), "0.0.0.0:8280", opts...)
+	if err != nil {
+		logger.Logger.Errorw("Failed to dial server", "failed to load key pair", err)
+		panic(err)
+	}
+
+	err = pbsas.RegisterStreamAuthServiceHandler(context.Background(), mux, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	return mux
 }
